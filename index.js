35494a5a56ade14a498b505b160f37dd62610d64
Kyle Munz on Snoopy <kyle138@gmail.com>
2016-10-31 19:26:42 -0500

    Updated code to handle SINGLE, DOUBLE, or LONG clicks and SMS accordingly.

--- index.js
+++ index.js
@@ -1,18 +1,69 @@
 'use strict';
 
-var aws = require('aws-sdk');
-aws.config.update({region:'us-west-2'});
+const AWS = require('aws-sdk');
+const SNS = new AWS.SNS( {apiVersion: '2010-03-31'} );
+
+var params = {};
+/**
+ * The following JSON template shows what is sent as the payload:
+{
+    "serialNumber": "GXXXXXXXXXXXXXXXXX",
+    "batteryVoltage": "xxmV",
+    "clickType": "SINGLE" | "DOUBLE" | "LONG"
+}
+ *
+ * A "LONG" clickType is sent if the first press lasts longer than 1.5 seconds.
+ * "SINGLE" and "DOUBLE" clickType payloads are sent for short clicks.
+ *
+ * For more documentation, follow the link below.
+ * http://docs.aws.amazon.com/iot/latest/developerguide/iot-lambda-rule.html
+ */
 
 exports.handler = (event, context, callback) => {
-  var sns = new aws.SNS({params: {TopicArn: 'arn:aws:sns:us-west-2:304786516436:checkMailButton'}});
+  //console.log('Received event:', event.clickType);    //DEBUG
 
-  sns.publish({Message: 'I checked the mail.'}, function (err, data) {
-    if (err) {
-      console.error("SNS Publish failed: "+err);
-    } else {
-      console.log("SNS Publish successful! "+ JSON.stringify(data,null,2));
-      callback(null, data.MessageId);
-    }
-  });
+  switch(event.clickType) {
+    case "SINGLE":
+      console.log("SINGLE Click");
+      params.PhoneNumber = '+1PHONENUMBER';
+      params.Message = 'Cathy checked the mail.';
+      SNS.publish(params, function (err, data) {
+        if (err) {
+          console.error("SNS Publish failed: "+err);
+        } else {
+          console.log("SNS Publish successful! "+ JSON.stringify(data,null,2));
+          callback(null, data.MessageId);
+        }
+      });
+      break;
+    case "DOUBLE":
+      console.log("DOUBLE CLICK");
+      params.PhoneNumber = '+1PHONENUMBER';
+      params.Message = 'Kyle checked the mail.';
+      SNS.publish(params, function (err, data) {
+        if (err) {
+          console.error("SNS Publish failed: "+err);
+        } else {
+          console.log("SNS Publish successful! "+ JSON.stringify(data,null,2));
+          callback(null, data.MessageId);
+        }
+      });
+      break;
+    case "LONG":
+      console.log("LONG CLICK");
+      params.PhoneNumber = '+1PHONENUMBER';
+      params.Message = 'Battery Voltage: '+event.batteryVoltage;
+      SNS.publish(params, function (err, data) {
+        if (err) {
+          console.error("SNS Publish failed: "+err);
+        } else {
+          console.log("SNS Publish successful! "+ JSON.stringify(data,null,2));
+          callback(null, data.MessageId);
+        }
+      });
+      break;
+    default:
+      console.log("NO CLICKS GIVEN");
+  }
 
 };
